42e60150c80367edd43e482f23fdbd369aef24a3
ruanyyyyyyy <katherineruany@gmail.com>
2021-03-27 14:46:43 -0700

    add spectral clustering

--- Homework3/compare_cluster.py
+++ Homework3/compare_cluster.py
@@ -17,6 +17,7 @@ from itertools import cycle, islice
 
 from KMeans import K_Means
 from GMM import GMM
+from spectral_clustering import spetral_clustering
 
 np.random.seed(0)
 
@@ -105,6 +106,7 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
     # 自编的K-Means、GMM算法
     my_kmeans = K_Means(n_clusters=params['n_clusters'])
     my_gmm = GMM(n_clusters=params['n_clusters'])
+    my_spectral = spetral_clustering(n_clusters=params['n_clusters'])
     # sklearn中自带的算法
     ms = cluster.MeanShift(bandwidth=bandwidth, bin_seeding=True)
     two_means = cluster.MiniBatchKMeans(n_clusters=params['n_clusters'])
@@ -130,6 +132,7 @@ for i_dataset, (dataset, algo_params) in enumerate(datasets):
     clustering_algorithms = (
         ('My_KMeans', my_kmeans),
         ('My_GMM', my_gmm),
+        ('My_spec', my_spectral),
         ('MiniBatchKMeans', two_means),
         ('AffinityPropagation', affinity_propagation),
         ('MeanShift', ms),
---
+++ Homework3/spectral_clustering.py
@@ -0,0 +1,48 @@
+import numpy as np
+from scipy.spatial import KDTree
+from scipy.stats import multivariate_normal
+import numpy.linalg as LA
+from sklearn.cluster import KMeans
+
+class spetral_clustering(object):
+    def __init__(self, n_clusters=2, nnk=3, normalized=True):
+        self.n_clusters=n_clusters
+        self.nnk_ = nnk
+        self.labels_ = np.empty(0)
+        self.normalized_ = normalized
+       
+    
+    def fit(self, data):
+        # get adjacency matrix W
+        m = data.shape[0]
+        tree = KDTree(data)
+        W = np.zeros((m,m))
+
+        for di, datum in enumerate(data):
+            ndists, ninds = tree.query([datum], self.nnk_+1)
+            
+            ninds = ninds[0]
+            ndists = ndists[0]
+            for ni, ndist in zip(ninds, ndists):
+                # the point itself would be among its knn. Skip
+                if ni==di:
+                    continue
+                W[di][ni] = W[ni][di] = 1/ndist
+
+        D = np.diag(W.sum(axis=1))
+        L = D - W
+        if self.normalized_:
+            L = np.matmul(LA.inv(D), L)
+        
+        eigvals, eigvecs = LA.eig(L)
+        sorted_idx = np.argsort(eigvals)
+        V = eigvecs[:, sorted_idx[:self.n_clusters]] # (n,k)
+        V = V.real #TODO:
+
+        self.labels_ = KMeans(n_clusters=self.n_clusters).fit_predict(V)
+    
+    def predict(self):
+        return self.labels_
+
+if __name__ == '__main__':
+    pass
