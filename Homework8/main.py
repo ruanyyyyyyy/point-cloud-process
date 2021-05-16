ae5339e6a18c71dd95939ea370af60cb78640cd1
ruanyyyyyyy <katherineruany@gmail.com>
2021-05-16 21:26:43 +0800

    finish

--- Homework8/main.py
+++ Homework8/main.py
@@ -3,18 +3,25 @@ import open3d as o3d
 from pyntcloud import PyntCloud
 from sklearn.neighbors import KDTree
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 B = 11
 
 def FPFH(dataWnormal, point_label, tree, radius, B):
+    '''
+    Parameters:
+        dataWnormal, point_label, tree, radius, B
+    Returns:
+        FPFH histograms
+    '''
     data = dataWnormal[:, :3]
     nei_hist, count = 0, 0
     nei_labels = tree.query_radius(data[point_label].reshape(1,-1), radius)[0]
     nei_labels = np.asarray(list(set(nei_labels) - set([point_label]))) # delete key point in neighbors
     histograms = SPFH(dataWnormal, point_label, tree, radius, B).astype(np.double)
-    for neighbor_label in nei_labels:
+    for neighbor_label in tqdm(nei_labels):
         count += 1
-        #TODO: weighted sum, calculate nei_hist
+        # weighted sum, calculate nei_hist
         wk = 1 / np.linalg.norm(data[point_label] - data[neighbor_label])
         nei_hist += wk * SPFH(dataWnormal, neighbor_label, tree, radius, B)
 
@@ -42,7 +49,7 @@ def SPFH(data_Wnormal, point_label, tree, radius, B):
     n1 = data_Wnormal[point_label][3:].reshape(1,-1)
     p1 = data_Wnormal[point_label][0:3].reshape(1,-1)
     for neighbor in local_points:
-        #TODO: calculate alpha, phi, theta
+        # calculate alpha, phi, theta
         n2 = neighbor[3:].reshape(1,-1)
         p2 = neighbor[0:3].reshape(1,-1)
         u = n1
@@ -60,16 +67,16 @@ def SPFH(data_Wnormal, point_label, tree, radius, B):
     histograms = np.hstack((alpha_hist, phi_hist, theta_hist)) # (33,)
     return histograms 
 
-
-
-
-if __name__=="__main__":
-    pc_path = "./data/chair/chair_0056.txt"
+def visulization(pc_path):
     point_cloud_pynt = PyntCloud.from_file(pc_path, sep=",", names=["x", "y", "z", "nx", "ny", "nz"])
     point_cloud_o3d = point_cloud_pynt.to_instance("open3d", mesh=False)
     pc_array = np.asarray(point_cloud_o3d.points)
     o3d.visualization.draw_geometries([point_cloud_o3d]) 
-    '''
+
+
+if __name__=="__main__":
+    pc_path = "./data/chair/chair_0056.txt"
+    
     with open(pc_path, 'r') as f:
         point_cloud = f.readlines()
     
@@ -81,13 +88,18 @@ if __name__=="__main__":
     pc_array = np.asarray(pc_list)
 
     tree = KDTree(pc_array[:,:3], leaf_size = 20)
-    histograms = FPFH(pc_array, 1, tree, 0.5, B)
+    histograms1 = FPFH(pc_array, 8628, tree, 0.3, B)
+    histograms2 = FPFH(pc_array, 1119, tree, 0.3, B)
+    histograms3 = FPFH(pc_array, 2696, tree, 0.3, B)
     
     plt.figure(figsize=(10,5))
     
     # x_coord = np.linspace(0, 32, 1)
     x_coord = list(range(33))
-    plt.plot(x_coord, histograms,color="deeppink", linewidth=2, linestyle=':', label='point1', marker='o')
+    plt.plot(x_coord, histograms1,color="deeppink", linewidth=2, linestyle=':', label='point1', marker='o')
+    plt.plot(x_coord, histograms2,color="blue", linewidth=2, linestyle=':', label='point2', marker='o')
+    plt.plot(x_coord, histograms3,color="green", linewidth=2, linestyle=':', label='point3', marker='o')
 
     plt.legend(loc=2)
-    plt.show()'''
+    plt.show()
+    
